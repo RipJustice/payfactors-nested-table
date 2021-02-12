0d3d47007c8946827152c048ff7c16445663684c
smullen <smullen@gmail.com>
2021-02-12 15:41:12 -0500

    adding import of mock data for service

--- src/app/services/mock.service.ts
+++ src/app/services/mock.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { JobData } from '../models/job-data';
+import { MockJobsData } from '../mocks/mock-jobs-data';
 
 @Injectable({
   providedIn: 'root'
